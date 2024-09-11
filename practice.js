792170d680857264d23f6826995a98fb85aa3abb
BinyaminYacovson <bj57577@gmail.com>
2024-09-11 15:17:49 +0300

    function 3

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
 // 1.
 function filterAndSortEvenNumbers(arr){
-    return arr.filter(num => num % 2 === 0).sort((a, b) => a - b);
+    return arr.filter(num => num % 2 === 0).sort()
 }
 
 // 2.
@@ -8,12 +8,23 @@ function removeDublicates(arr){
     const result = []
     for (num of arr) {
         if (!result.includes(num)) {
-            result.push(num);
+            result.push(num)
         }
     }
     return result;
 }
 
 // 3.
-//function capitalizeFirstLetter(str)
+function capitalizeFirstLetter(str){
+    arr = str.split(' ')
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].charAt(arr[i].length - 1) !== '.') {
+            arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1)
+        }
+    }
+    return arr.join(' ')
+}
+
+// 4.
+ 
 
